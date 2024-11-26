9c5876ca6c48f39d76eafcebfe62bf8f1c5a9045
maxjdev <maxjramosdev@gmail.com>
2024-11-26 16:57:28 -0300

    feat: adiciona consulta por email no repository

--- src/main/java/br/com/forum/domain/usuario/UsuarioRepository.java
+++ src/main/java/br/com/forum/domain/usuario/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package br.com.forum.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     boolean existsByEmail(String email);
+
+    UserDetails findByEmail(String email);
 }
